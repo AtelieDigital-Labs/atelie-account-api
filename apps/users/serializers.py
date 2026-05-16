140aae1028b30141d4fe23f561e6ccedb0d82e6a
Aroldo Fernandes <aroldo.caval@gmail.com>
2026-05-16 09:02:39 -0300

    feat: validate CEP, integrate with ViaCEP and validate CPF

--- apps/users/serializers.py
+++ apps/users/serializers.py
@@ -2,7 +2,8 @@ from rest_framework import serializers
 from dj_rest_auth.registration.serializers import RegisterSerializer
 from . import models
 from datetime import date
-
+import httpx
+from rest_framework.validators import UniqueValidator
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
@@ -10,13 +11,14 @@ class UserSerializer(serializers.ModelSerializer):
         fields = ['id', 
                   'username',
                   'email',
-                  'name',
+                  'first_name',
+                  'last_name',
                   'cpf',
                   'bio',
+                  'profile_image',
                   ]
-        extra_kwargs = {'cpf': {'read_only': True}}
         read_only_fields = ['cpf',]
-    
+        
 class AddressSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.UserAddress
@@ -33,7 +35,48 @@ class AddressSerializer(serializers.ModelSerializer):
             'is_main'
         ]
         read_only_fields = ['user',]
+    
+    def validate_zip_code(self, value):
+        zip_code = "".join(filter(str.isdigit, value))
 
+        if len(zip_code) != 8:
+            raise serializers.ValidationError("O CEP deve conter exatamente 8 dígitos.")
+
+        return zip_code
+    
+    def validate(self, attrs):
+        zip_code = attrs.get("zip_code")
+        
+        try:
+            with httpx.Client(timeout=5.0) as client:
+                response = client.get(f"https://viacep.com.br/ws/{zip_code}/json/")
+            
+            if response.status_code != 200:
+                raise serializers.ValidationError({"zip_code": "Serviço de busca de CEP indisponível."})
+
+            viacep_data = response.json()
+            
+            if viacep_data.get('erro'):
+                raise serializers.ValidationError({"zip_code": "CEP inexistente."})
+
+        except httpx.RequestError:
+            raise serializers.ValidationError({"zip_code": "Erro de conexão ao validar o CEP."})
+
+        # Mapeamento
+        mapping = {
+            "street": "logradouro",
+            "neighborhood": "bairro",
+            "city": "localidade",
+            "state": "uf",
+        }
+
+        for field, viacep_field in mapping.items():
+            val = viacep_data.get(viacep_field)
+            if val:
+                attrs[field] = val
+
+        return attrs
+    
 class BecomeArtisanSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.User
@@ -43,29 +86,56 @@ class BecomeArtisanSerializer(serializers.ModelSerializer):
 
 
 class CustomRegisterSerializer(RegisterSerializer):
-    name = serializers.CharField(required=True)
+    email = serializers.EmailField(
+        required=True,
+        validators=[UniqueValidator(queryset=models.User.objects.all(), message="Este e-mail já está em uso.")]
+    )
+    first_name = serializers.CharField(required=True)
+    last_name = serializers.CharField(required=True)
     phone_number = serializers.CharField(required=False, allow_blank=True)
-    cpf = serializers.CharField(required=False, allow_blank=True)
+    cpf = serializers.CharField(
+        required=False, 
+        allow_blank=True, 
+        validators=[UniqueValidator(queryset=models.User.objects.all(), message="Este CPF já está em uso.")]
+    )
     date_of_birth = serializers.DateField(required=False)
 
-    # def validate_date_of_birth(self, value):
-    #     today = date.today()
+    def validate_cpf(self, value):
+        cpf = "".join(filter(str.isdigit, value))
 
-    #     age = today.year - value.year - (
-    #         (today.month, today.day) < (value.month, value.day)
-    #     )
+        if len(cpf) != 11:
+             raise serializers.ValidationError("O CPF deve conter exatamente 11 dígitos.")
+        
+        from validate_docbr import CPF
+        cpf_validator = CPF()
 
-    #     if age < 18:
-    #         raise serializers.ValidationError(
-    #             "Você precisa ser maior de idade para se cadastrar."
-    #         )
+        if not cpf_validator.validate(cpf):
+            raise serializers.ValidationError("CPF inválido.")
+        
+        return cpf
+    
+    def validate_date_of_birth(self, value):
+        today = date.today()
+        
+        if value > today:
+            raise serializers.ValidationError("A data de nascimento não pode ser no futuro.")
 
-    #     return value
+        AGE_OF_MAJORITY = 18
+        try:
+            limit_date = date(today.year - AGE_OF_MAJORITY, today.month, today.day)
+        except ValueError: # Tratamento para anos bissextos
+            limit_date = date(today.year - AGE_OF_MAJORITY, today.month, today.day - 1)
+
+        if value > limit_date:
+            raise serializers.ValidationError("Você deve ter pelo menos 18 anos.")
+        
+        return value
 
     def get_cleaned_data(self):
         data = super().get_cleaned_data()
         data.update({
-            'name': self.validated_data.get('name', ''),
+            'first_name': self.validated_data.get('first_name', ''),
+            'last_name': self.validated_data.get('last_name', ''),
             'phone_number': self.validated_data.get('phone_number', ''),
             'cpf': self.validated_data.get('cpf', ''),
             'date_of_birth': self.validated_data.get('date_of_birth'),
@@ -75,10 +145,12 @@ class CustomRegisterSerializer(RegisterSerializer):
     def save(self, request):
         user = super().save(request)
 
-        user.name = self.validated_data.get('name', '')
+        user.first_name = self.validated_data.get('first_name')
+        user.last_name = self.validated_data.get('last_name')
         user.phone_number = self.validated_data.get('phone_number')
         user.cpf = self.validated_data.get('cpf')
         user.date_of_birth = self.validated_data.get('date_of_birth')
 
         user.save()
         return user
+    
