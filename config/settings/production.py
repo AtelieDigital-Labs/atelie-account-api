a2340f38c79f96cb107e7b095f17bf7d238344e1
Aroldo Fernandes <aroldo.caval@gmail.com>
2026-06-28 14:26:44 -0300

    feat: new sintaxi dj-rest-auth

--- config/settings/production.py
+++ config/settings/production.py
@@ -6,5 +6,6 @@ ALLOWED_HOSTS = env.list("ALLOWED_HOSTS")
 
 DATABASES = {"default": env.db("DATABASE_URL")}
 
-ACCOUNT_EMAIL_REQUIRED = True
+# ACCOUNT_EMAIL_REQUIRED = True
+ACCOUNT_SIGNUP_FIELDS = ['email*', 'username*', 'password1*', 'password2*']
 ACCOUNT_EMAIL_VERIFICATION = "mandatory"
